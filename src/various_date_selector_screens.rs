d5df47f82e4acf935f2f15c88a732db93a8afbcd
gus-eisen <guseisenberch@proton.me>
2025-10-20 19:00:01 -0500

    Added various_date_selector_screens.rs

---
+++ src/various_date_selector_screens.rs
@@ -0,0 +1,42 @@
+mod day_view_screen;
+mod event_editor_screen;
+mod objects;
+
+use pelican_ui::drawable::{Align, Component, Drawable};
+use pelican_ui::events::OnEvent;
+use pelican_ui::layout::{Area, Layout, SizeRequest};
+use pelican_ui::{Application, Component, Context, Plugin, Plugins, start};
+use std::collections::BTreeMap;
+
+use pelican_ui_std::AppPage;
+use pelican_ui_std::components::button::Button;
+use pelican_ui_std::components::interface::general::{Content, Header, Interface, Page};
+use pelican_ui_std::components::{ExpandableText, Text, TextStyle};
+use pelican_ui_std::events::NavigateEvent;
+use pelican_ui_std::layout::{Column, Offset, Padding, Size};
+
+use crate::event_editor_screen::EventEditorScreen;
+
+#[derive(Debug, Component)]
+pub struct YearSelectorScreen(Column, Page, #[skip] String);
+
+impl OnEvent for YearSelectorScreen {}
+
+impl AppPage for MonthScreen {
+    // This screen does not have a navigation bar
+    fn has_nav(&self) -> bool {
+        false
+    }
+
+    // Handle page navigation. Always returns Err(self) because this page cannot navigate.
+    fn navigate(
+        self: Box<Self>,
+        ctx: &mut Context,
+        index: usize,
+    ) -> Result<Box<dyn AppPage>, Box<dyn AppPage>> {
+        match index {
+            0 => return Ok(Box::new(EventEditorScreen::new(ctx))),
+            _ => Err(self),
+        }
+    }
+}
