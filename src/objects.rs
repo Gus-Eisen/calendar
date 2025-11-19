2722d8cdf6341c053e4ff98e04bbe9eb12080bc3
gus-eisen <guseisenberch@proton.me>
2025-11-19 17:04:32 -0600

    Added Month.as_u32().

--- src/objects.rs
+++ src/objects.rs
@@ -43,6 +43,23 @@ impl Month {
             Month::December => "December",
         }
     }
+
+    pub fn as_u32(&self) -> u32 {
+        match self {
+            Month::January => 1,
+            Month::February => 2,
+            Month::March => 3,
+            Month::April => 4,
+            Month::May => 5,
+            Month::June => 6,
+            Month::July => 7,
+            Month::August => 8,
+            Month::September => 9,
+            Month::October => 10,
+            Month::November => 11,
+            Month::December => 12,
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -99,7 +116,11 @@ impl EventForEES {
             .clone()
             .unwrap()
             .parse::<u32>()
-            .expect("Invalid day.")
+            .expect("Unable to parse Day from EventForEES")
+    }
+
+    pub fn get_month_as_u32(&self) -> u32 {
+        self.month.clone().unwrap().as_u32()
     }
 
     pub fn all_some(&self) -> bool {
