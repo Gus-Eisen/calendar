282e1fa48a626fbfa3f5c1ec77b353cf24c7ced7
gus-eisen <guseisenberch@proton.me>
2026-01-08 14:08:46 -0600

    DayViewScreen struct and constructor added.

---
+++ src/day_view_screen.rs
@@ -0,0 +1,48 @@
+use pelican_ui::components::button::PrimaryButton;
+use pelican_ui::components::interface::general::{Content, Header, Page};
+use pelican_ui::components::interface::navigation::{AppPage, NavigationEvent};
+use pelican_ui::events::OnEvent;
+use pelican_ui::layouts::{Column, Offset};
+use pelican_ui::layouts::{Padding, Size};
+use pelican_ui::{Component, Context};
+
+#[derive(Debug, Component)]
+pub struct DayViewScreen(Column, Page);
+
+impl OnEvent for DayViewScreen {}
+
+impl AppPage for DayViewScreen {}
+
+impl DayViewScreen {
+    pub fn new(ctx: &mut Context) -> Result<Self, String> {
+        let header = Header::home(
+            // The majority of UI components will require the app context.
+            ctx,
+            "placeholder",
+            None, // There will not be an icon button on this header
+        );
+
+        let ret_to_cal_button = PrimaryButton::new(
+            ctx,
+            "Return to Calendar",
+            |ctx: &mut Context| {
+                ctx.trigger_event(NavigationEvent::Pop);
+                println!("Return to Calendar button clicked.")
+            },
+            false,
+        );
+
+        // Combine icon, heading, and subtext into page content
+        let content = Content::new(
+            ctx,
+            Offset::Start,
+            // All items must be boxed as Box<dyn Drawable>
+            vec![Box::new(ret_to_cal_button)],
+        );
+
+        Ok(Self(
+            Column::new(1.0, Offset::Start, Size::Fit, Padding::new(1.0)),
+            Page::new(header, content, None),
+        ))
+    }
+}
